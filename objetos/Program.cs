f4718ca366152799448dbba085bc4a09ef2d3650
LeoRdgz <leonardorodriggss@gmail.com>
2023-05-16 12:24:15 -0500

    Primer repositorio de GitHub

---
+++ objetos/Program.cs
@@ -0,0 +1,32 @@
+﻿using objetos.clases;
+using System;
+
+namespace objetos
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            Persona persona = new Persona();
+
+            persona.Nombre = "Maria";
+            persona.Apellido = "Sosa";
+            persona.Edad = 25;
+            persona.Salario = 12313.33; 
+            
+            persona.ImprimirDatos();
+
+            Console.WriteLine("--------------");
+            Persona persona2 = new Persona();
+            persona.Nombre = "Jose";
+            persona.Apellido = "Alegria";
+            persona.Edad = 19;
+            persona.Salario = 13000.33;
+
+            persona2.ImprimirDatos();
+         
+        }
+    }
+}
---
+++ objetos/clases/Persona.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace objetos.clases
+{
+    //clase es una plantlla, que contiene objetos y métodos 
+    public class Persona
+    {
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public int Edad { get;set; }
+        public double Salario { get; set; } //double por su tamaño de la variable
+
+        public void ImprimirDatos()
+        {
+            Console.WriteLine("Tu nombre es:" + this.Nombre);
+            Console.WriteLine("Tu apellido es:" + this.Apellido);
+            Console.WriteLine("Tu edad es:" + this.Edad);
+            Console.WriteLine("Tu salario es:" + this.Salario);
+        }
+
+    }
+}
